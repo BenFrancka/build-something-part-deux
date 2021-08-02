15b338aa4e2da909bae62cd8b4f0e5cdf703948f
BenFrancka <benfrancka@gmail.com>
2021-08-02 16:04:20 -0700

    adds initial failing test for POST route to corporations table

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,8 +3,25 @@ import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
 
-describe('demo routes', () => {
+describe('corporations routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('creates a corporation in the database with POST', async () => {
+    const spacersChoice = {
+      companyName: 'spacers choice',
+      parentCompany: 'universal defense logistics',
+      slogan: 'its not the best choice, its spacers choice',
+    };
+
+    const res = await request(app)
+      .post('/api/v1/corporations')
+      .send(spacersChoice);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...spacersChoice,
+    });
   });
 });
