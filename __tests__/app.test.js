f6a29580f535dcb787270442f534089da822abd1
BenFrancka <benfrancka@gmail.com>
2021-08-02 16:29:17 -0700

    adds initial failing get by id test for corporations routes to app.test.js

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Corporation from '../lib/models/Corporation.js';
 
 describe('corporations routes', () => {
   beforeEach(() => {
@@ -23,5 +24,19 @@ describe('corporations routes', () => {
       id: '1',
       ...spacersChoice,
     });
+  });
+
+  it('gets a corporation from the database by id  with GET', async () => {
+    const spacersChoice = await Corporation.insert({
+      companyName: 'spacers choice',
+      parentCompany: 'universal defense logistics',
+      slogan: 'its not the best choice, its spacers choice',
+    });
+
+    const res = await request(app).get(
+      `/api/v1/corporations/${spacersChoice.id}`
+    );
+
+    expect(res.body).toEqual(spacersChoice);
   });
 });
