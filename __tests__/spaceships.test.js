f6a548f1a79eec11bdc189e420ba8c3da0659943
BenFrancka <benfrancka@gmail.com>
2021-08-02 17:44:29 -0700

    adds initial failing test to spaceships.test.js for POST route

---
+++ __tests__/spaceships.test.js
@@ -0,0 +1,29 @@
+import pool from '../lib/utils/pool.js';
+import setup from '../data/setup.js';
+import request from 'supertest';
+import app from '../lib/app.js';
+
+describe('spaceships routes', () => {
+  beforeEach(() => {
+    return setup(pool);
+  });
+
+  it('creates a spaceship in the database with POST', async () => {
+    const rocinante = {
+      shipName: 'Rocinante',
+      shipSize: '46 meters',
+      captainName: 'James Holden',
+      fictionalUniverse: 'The Expanse',
+      crewSize: 5,
+    };
+
+    const res = await request(app)
+      .post('/api/v1/spaceships')
+      .send(rocinante);
+
+    expect(res.body).toEqual({
+      id: '1',
+      ...rocinante,
+    });
+  });
+});
